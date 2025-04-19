b9abc50da3bc3f86370be7ce4848ec130a3fd240
Natali Ustinova <nataliaustinova58@gmail.com>
2025-04-19 11:34:47 +0300

    feat: backend routes

---
+++ backend/src/routes/conversionRoutes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { convertToRoman, convertToArabic, getAll, removeAll } from '../controllers/conversionController';
+
+const router = Router();
+
+// Route for converting Arabic to Roman
+router.get('/roman/:inputValue', convertToRoman);
+
+// Route for converting Roman to Arabic
+router.get('/arabic/:inputValue', convertToArabic);
+
+// Route for getting all conversions
+router.get('/all', getAll);
+
+// Route for removing all conversions
+router.delete('/remove', removeAll);
+
+export default router;
