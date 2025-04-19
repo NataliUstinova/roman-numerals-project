863f12de01c5786cf92f3db66d6cb444d074fa95
Natali Ustinova <nataliaustinova58@gmail.com>
2025-04-19 14:25:07 +0300

    feat: cors options added

--- backend/src/middlewares/security/cors.ts
+++ backend/src/middlewares/security/cors.ts
@@ -2,5 +2,12 @@ import cors from 'cors';
 import { Express } from 'express';
 
 export const setupCors = (app: Express): void => {
-  app.use(cors());
+  const corsOptions = {
+    origin: process.env.NODE_ENV === 'production'
+          ? process.env.ALLOWED_ORIGINS?.split(',') // || 'https://domain.com' TODO add domain when prod is set
+          : '*',
+          methods: ['GET', 'POST', 'DELETE'],
+          allowedHeaders: ['Content-Type', 'Authorization']
+      };
+  app.use(cors(corsOptions));
 };
